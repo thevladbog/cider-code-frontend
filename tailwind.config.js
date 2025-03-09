c05b8f5c81b666e634fc9f34566012d4ded5a0ec
Vladislav Bogatyrev <vladislav.bogatyrev@gmail.com>
2025-03-09 19:00:35 +0300

    first init 2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
-import {heroui} from "@heroui/theme"
+import { heroui } from "@heroui/theme";
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./index.html",
-    './src/layouts/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/layouts/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
@@ -14,4 +14,4 @@ module.exports = {
   },
   darkMode: "class",
   plugins: [heroui()],
-}
+};

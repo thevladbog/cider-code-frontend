b563e56cd3508cf054ec92f8bc6d95be154b5781
Vladislav Bogatyrev <vladislav.bogatyrev@gmail.com>
2025-05-08 23:53:13 +0300

    fix: deleted fragment in Footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,25 +4,23 @@ import s from "./Footer.module.scss";
 
 export const FooterWrapper = () => {
   return (
-    <>
-      <Footer
-        className={s.footer}
-        withDivider={true}
-        moreButtonTitle="Show more"
-        copyright={`@ ${new Date().getFullYear()} "BOTTLE [CODE]"`}
-        menuItems={[
-          {
-            text: "Поддержка",
-            href: "https://gravity-ui.com/",
-            target: "blank",
-          },
-          {
-            text: "Конфиденциальность",
-            href: "https://gravity-ui.com/",
-            target: "blank",
-          },
-        ]}
-      />
-    </>
+    <Footer
+      className={s.footer}
+      withDivider={true}
+      moreButtonTitle="Show more"
+      copyright={`@ ${new Date().getFullYear()} "BOTTLE [CODE]"`}
+      menuItems={[
+        {
+          text: "Поддержка",
+          href: "https://gravity-ui.com/",
+          target: "blank",
+        },
+        {
+          text: "Конфиденциальность",
+          href: "https://gravity-ui.com/",
+          target: "blank",
+        },
+      ]}
+    />
   );
 };
